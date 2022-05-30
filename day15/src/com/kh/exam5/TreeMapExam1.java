e87a1a7bc85edcbc5559bb864f86deac012e8077
sanghakyi <sang105kr@gmail.com>
2022-05-30 16:32:13 +0900

    220530-7

---
+++ day15/src/com/kh/exam5/TreeMapExam1.java
@@ -0,0 +1,47 @@
+package com.kh.exam5;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class TreeMapExam1 {
+  public static void main(String[] args) {
+    TreeMap<Integer, String> scores = new TreeMap<>();
+    scores.put(87, "홍길동1");
+    scores.put(98, "홍길동2");
+    scores.put(75, "홍길동3");
+    scores.put(95, "홍길동4");
+    scores.put(80, "홍길동5");
+    System.out.println(scores);
+
+    Map.Entry<Integer, String> entry = scores.firstEntry();
+    System.out.println(entry.getKey() + ":" + entry.getValue());
+
+    entry = scores.lastEntry();
+    System.out.println(entry.getKey() + ":" + entry.getValue());
+
+    entry = scores.higherEntry(95);
+    System.out.println(entry.getKey() + ":" + entry.getValue());
+
+    entry = scores.lowerEntry(95);
+    System.out.println(entry.getKey() + ":" + entry.getValue());
+
+    entry = scores.ceilingEntry(95);
+    System.out.println(entry.getKey() + ":" + entry.getValue());
+
+    entry = scores.floorEntry(95);
+    System.out.println(entry.getKey() + ":" + entry.getValue());
+
+//    while (!scores.isEmpty()) {
+//      entry = scores.pollFirstEntry();
+//      System.out.println(entry.getKey() + ":" + entry.getValue() + ":" + scores.size());
+//    }
+
+    //검색
+    SortedMap<Integer, String> subMap = scores.subMap(80, 90);
+    System.out.println(subMap);
+    NavigableMap<Integer, String> subMap2 = scores.subMap(80, false, 90, false);
+    System.out.println(subMap2);
+  }
+}
---
+++ day15/src/com/kh/exam5/TreeSetExam1.java
@@ -0,0 +1,48 @@
+package com.kh.exam5;
+
+import java.util.NavigableSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class TreeSetExam1 {
+  public static void main(String[] args) {
+    TreeSet<Integer> scores = new TreeSet<>();
+    scores.add(87);
+    // = scores.add(new Integer(87));
+    scores.add(98);
+    scores.add(75);
+    scores.add(95);
+    scores.add(80);
+    System.out.println(scores);
+
+    Integer score = null;
+    score = scores.first();
+    System.out.println("가장낮은 점수 : " + score);    
+    score = scores.last();
+    System.out.println("가장높은 점수 : " + score);
+    score = scores.lower(87);
+    System.out.println("87보다 낮은 점수 : " + score);
+    score = scores.higher(87);
+    System.out.println("87보다 높은 점수 : " + score);
+
+    score = scores.floor(87);
+    System.out.println("87점이거나 87보다 낮은 점수 : " + score);
+
+    score = scores.ceiling(87);
+    System.out.println("87점이거나 87보다 높 점수 : " + score);
+
+//    while (!scores.isEmpty()) {
+//      score = scores.pollLast();
+//      System.out.println(score + "남은 객체 수 : " + scores.size());
+//    }
+
+    NavigableSet<Integer> integers = scores.descendingSet();
+    System.out.println(integers);
+
+    //범위검색
+    SortedSet<Integer> integers1 = scores.subSet(80, 90);
+    System.out.println(integers1);
+    NavigableSet<Integer> integers2 = scores.subSet(80, false, 95, true);
+    System.out.println(integers2);
+  }
+}
